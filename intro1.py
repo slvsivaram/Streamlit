9b1b866c7e6f651df389fd44c481062f1d6a5878
slvsivaram <40560750+slvsivaram@users.noreply.github.com>
2021-11-04 11:54:19 +0530

    Update intro1.py

--- intro1.py
+++ intro1.py
@@ -10,9 +10,9 @@ conn = connect()
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
+#def run_query(query):
+#    rows = conn.execute(query, headers=1)
+#    return rows
 
 sheet_url = st.secrets["https://docs.google.com/spreadsheets/d/1XBGtUKORl0QBaUXeshFhNAMj5tSbaDRyFUU9PaGtUIY/edit#gid=1393963821"]
 rows = conn.execute(f'SELECT * FROM "{sheet_url}"')
