ff6eff98bce41253c5562b51cc9b29c21514fbdd
slvsivaram <40560750+slvsivaram@users.noreply.github.com>
2021-11-04 12:06:50 +0530

    Update intro1.py

--- intro1.py
+++ intro1.py
@@ -5,16 +5,19 @@ import pandas as pd
 from gsheetsdb import connect
 
 # Create a connection object.
-conn = connect()
+
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
+# @st.cache(ttl=600)
 #def run_query(query):
 #    rows = conn.execute(query, headers=1)
 #    return rows
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1XBGtUKORl0QBaUXeshFhNAMj5tSbaDRyFUU9PaGtUIY/edit?usp=sharing"
+
+conn = connect()
+
 rows = conn.execute(f'SELECT * FROM "{sheet_url}"')
 df = pd.DataFrame(rows)
 st.write(df)
