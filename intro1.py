1aefb055f8a4809f66dd1961c8ec9f433b295189
slvsivaram <40560750+slvsivaram@users.noreply.github.com>
2021-11-13 10:42:46 +0530

    Update intro1.py

--- intro1.py
+++ intro1.py
@@ -34,7 +34,7 @@ st.title ("Ojas Target")
 option = st.sidebar.selectbox('Select BA',df1.BA)
 df1.set_index("BA", inplace = True)
 result = df1.loc[[option]]
-st.write( result1)
+st.write( result)
 
 option1 = st.sidebar.selectbox('Select Qtr',("Q1","Q2","Q3","Q4") )
 df2 = pd.DataFrame(rows)
