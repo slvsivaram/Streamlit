18aa3ad57bb80c597fc9cce4bf42ebb5903da76b
slvsivaram <40560750+slvsivaram@users.noreply.github.com>
2021-11-14 07:27:49 +0530

    Update intro1.py

--- intro1.py
+++ intro1.py
@@ -41,8 +41,10 @@ df2 = pd.DataFrame(rows)
 result1 = df2[["BA",option1]]
 st.write( result1)
 
+Ojas Target = st.sidebar.button('Ojas Target')
+if st.button('Ojas Target'):
+    st.write(df1)
 
-st.write(df1)
 
 # Print results.
 #for row in rows:
