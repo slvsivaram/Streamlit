60246e5a85ff6874aa31212793f47acf9dd53203
slvsivaram <40560750+slvsivaram@users.noreply.github.com>
2021-12-27 20:32:26 +0530

    Update intro1.py

--- intro1.py
+++ intro1.py
@@ -27,8 +27,8 @@ st.write(df)
 sheet_url1 = "https://docs.google.com/spreadsheets/d/1XBGtUKORl0QBaUXeshFhNAMj5tSbaDRyFUU9PaGtUIY/edit#gid=1393963821"
 
 
-rows = conn.execute(f'SELECT * FROM "{sheet_url1}"')
-df1 = pd.DataFrame(rows)
+rows1 = conn.execute(f'SELECT * FROM "{sheet_url1}"')
+df1 = pd.DataFrame(rows1)
 st.title ("Ojas Target")
 
 option = st.sidebar.selectbox('Select BA',df1.BA)
@@ -38,7 +38,7 @@ st.write( result)
 #st.write( "sum : ", result.sum())
 
 option1 = st.sidebar.selectbox('Select Qtr',("Q1","Q2","Q3","Q4") )
-df2 = pd.DataFrame(rows)
+df2 = pd.DataFrame(rows1)
 result1 = df2[["BA",option1]]
 st.write( result1)
 
@@ -46,7 +46,7 @@ st.write( result1)
 if st.sidebar.button('Ojas Target'):
     st.write(df1)
 st.line_chart(df1)
-df4 = df1[["BA","Total"]]
+df4 = df2[["BA","Total"]]
 st.line_chart(df4)
 
 # Print results.
